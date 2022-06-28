40862c0b981ffaffbb72e3075260149217b9d033
Pedro Rocha Ferreira <pedro.Ferreira@lead.fortaleza>
2022-06-28 15:58:02 -0300

    Creating Home and Navbar component

---
+++ src/components/Navbar.vue
@@ -0,0 +1,12 @@
+<template>
+  <nav class="navbar">
+    <router-link to="/">Home</router-link> |
+    <router-link to="/products">Products</router-link>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: 'Navbar'
+}
+</script>
