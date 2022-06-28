49b31a65b126cf425ece16c90b2c14e3b9ec2632
Pedro Rocha Ferreira <pedro.Ferreira@lead.fortaleza>
2022-06-28 13:13:42 -0300

    Adding toast feature to project

--- src/main.js
+++ src/main.js
@@ -1,7 +1,10 @@
 import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
+
 import PrimeVue from 'primevue/config';
+import ToastService from 'primevue/toastservice';
+import Toast from 'primevue/toast';;
 
 import 'primevue/resources/themes/saga-blue/theme.css';
 import 'primevue/resources/primevue.min.css';
@@ -11,5 +14,8 @@ const app = createApp(App);
 
 app.use(router);
 app.use(PrimeVue);
+app.use(ToastService);
+
+app.component('Toast', Toast);
 
 app.mount('#app');
