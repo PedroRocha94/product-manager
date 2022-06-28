65c17709bb7caa6f76789e1b3a199a60ab11248a
Pedro Rocha Ferreira <pedro.Ferreira@lead.fortaleza>
2022-06-28 15:55:18 -0300

    Creating Product Component

---
+++ src/pages/Product.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="product">
+    <Navbar/>
+    <h1>Products</h1>
+  </div>
+</template>
+
+<script>
+import Navbar from '../components/Navbar.vue';
+
+export default {
+  name: 'Products',
+  components: {
+    Navbar
+  }
+}
+</script>
