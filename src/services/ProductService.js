9767d0ba93c08bc8aa3d70f1697ecad5c12202a5
Pedro Rocha Ferreira <pedro.Ferreira@lead.fortaleza>
2022-07-05 14:15:17 -0300

    Creating editProduct service in ProductService

--- src/services/ProductService.js
+++ src/services/ProductService.js
@@ -3,9 +3,14 @@ import axios from 'axios';
 const baseUrl = 'http://localhost:8080/api/v1/products';
 
 export function getAllProducts(){
-  return axios.get(baseUrl);
+  return axios.get(baseUrl + '?isActive=true');
 }
 
 export function postProduct(product){
   return axios.post(baseUrl, product);
+}
+
+export function editProduct(id, product){
+  let url = baseUrl + `/${id}`;
+  return axios.put(url, product);
 }
