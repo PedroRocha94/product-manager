2acfca6b293fa787baf39cda4c0d34853cda6914
Pedro Rocha Ferreira <pedro.Ferreira@lead.fortaleza>
2022-07-05 14:57:36 -0300

    Adding text to the About page

--- src/pages/About.vue
+++ src/pages/About.vue
@@ -2,6 +2,10 @@
   <Navbar/>
   <div>
     <h1>About</h1>
+    <p>This project was developed from a challenge created by Rogério Silva 
+      {link github} where an API of a CRUD of products was consumed.
+      {repository link}
+    </p>
   </div>
 </template>
 
